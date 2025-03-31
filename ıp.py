da39069f96227f0ebacb80e707caffe5f24d0656
serdarera11 <kralserdarok@gmail.com>
2025-03-31 15:50:24 +0300

    Add files via upload

---
+++ ıp.py
@@ -0,0 +1,33 @@
+import time
+print("İstediğiniz cihaza sızın!")
+time.sleep(2)
+ # Kullanıcıdan IP adresi almak
+input("Ip Adresi (sızmak istediğiniz cihazın):")
+time.sleep(2) 
+# Port taraması simülasyonu
+print("[+]Portlar taranıyor..")
+time.sleep(3)
+print("[+][+]Açık port bulundu: EternalBlue")
+time.sleep(2)
+ # Sızma simülasyonu
+print("[+]Porttan sızılıyor..")
+time.sleep(3)
+print("[+]Başarı ile sızıldı! Veri tabanı çekiliyor..")
+time.sleep(3)
+ # Sistem bilgisi simülasyonu
+print("[+]Sistem bilgisi: Windows 11 pro")
+time.sleep(1)
+print("[+]Password: 9651411814")
+time.sleep(2)
+ # Dosya yükleme simülasyonu
+input("(1/1)Yüklemek İstediğiniz Dosyayı Girin: ")
+time.sleep(2)
+print("[+]Dosya Başarı İle Yüklendi!")
+time.sleep(2)
+    # Güvenlik önlemleri hakkında bilgi
+print("Güvenlik duvarı Oturumu sonlandırıyor..")
+time.sleep(2)
+print("Güvenlik duvarı aşılmaya çalışıyor..")
+time.sleep(2)
+print("İşlem başarısız.Oturum sonlandırıldı")
+time.sleep(2)
